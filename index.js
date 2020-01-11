1f2057b9cb975057fb9389d6a13ab017dc9b3683
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-12 00:43:35 +0100

    refactored provider

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import NavigationWrapper from './app/navigation/navigation-wrapper'
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => NavigationWrapper);
+AppRegistry.registerComponent(appName, () => App);
