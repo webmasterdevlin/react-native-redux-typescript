823864fca1a8e7d083e5b6b695f785bb811902f5
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-23 17:56:52 +0100

    replaced getParams with route.params

--- app/foods/screens/FoodDetail.tsx
+++ app/foods/screens/FoodDetail.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Text} from 'react-native-paper';
 
-const FoodDetail: React.FC<any> = props => (
-  <Text style={{fontSize: 40}}>{props.navigation.getParam('obj').name}</Text>
-);
+const FoodDetail: React.FC<any> = props => {
+  const {obj} = props.route.params;
+  return <Text style={{fontSize: 40}}>{obj.name}</Text>;
+};
 
 export default FoodDetail;
--- app/todos/screens/TodoDetail.tsx
+++ app/todos/screens/TodoDetail.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {Text} from 'react-native-paper';
 
 const TodoDetail: React.FC<any> = props => {
-  return (
-    <Text style={{fontSize: 30}}>{props.navigation.getParam('obj').title}</Text>
-  );
+  const {obj} = props.route.params;
+  return <Text style={{fontSize: 30}}>{obj.title}</Text>;
 };
 export default TodoDetail;
